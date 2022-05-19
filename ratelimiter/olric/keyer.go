de4b9a3d5d14f94a6c512206277c68f0bc416f64
Frederik Schöll <fschoell@users.noreply.github.com>
2022-05-19 16:09:47 +0200

    fix gitignore, remove olric ratelimiter

--- ratelimiter/olric/keyer.go
+++
@@ -1,8 +0,0 @@
-package ratelimiter
-
-import "fmt"
-
-// RCC:[PREFIX]:[UID]:[TIMESTAMP_MINUTE]
-func requestConsumptionCounter(prefix, uid, timestampMinute string) string {
-	return fmt.Sprintf("RCC:%s:%s:%s", prefix, uid, timestampMinute)
-}
