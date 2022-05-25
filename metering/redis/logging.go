4d74988bd306c522cce8347a6965852cdcd0172d
Frederik Schöll <fschoell@users.noreply.github.com>
2022-05-25 13:07:29 +0200

    fix logger init for redis metrics (#6)

--- metering/redis/logging.go
+++ metering/redis/logging.go
@@ -2,11 +2,6 @@ package redis
 
 import (
 	"github.com/streamingfast/logging"
-	"go.uber.org/zap"
 )
 
-var zlog = zap.NewNop()
-
-func init() {
-	logging.Register("github/dfuse-io/dauth/metering/redis", &zlog)
-}
+var zlog, _ = logging.PackageLogger("dauth", "github.com/streamingfast/dauth/authenticator/middleware")
